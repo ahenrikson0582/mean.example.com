8f86406da77b7ac51b156ed4f6211f1916ef0666
Abby Henrikson <apeskind999@gmail.com>
2021-09-27 11:49:50 -0500

    Connect to the database

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var config = require('./config.dev');
 
-//Test the file
-
+var mongoose = require('mongoose');
 
 var app = express();
-
+mongoose.connect(config.mongodb, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
